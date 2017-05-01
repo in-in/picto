b8f5b90518ecf838124f7370f56d4c598100f753
in-in <0866222@gmail.com>
2017-05-01 03:44:16 +0300

    init commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,68 @@
+import gulp from 'gulp';
+import del from 'del';
+import browserSync from 'browser-sync';
+import pug from 'gulp-pug';
+import mjml from 'gulp-mjml';
+import notify from 'gulp-notify';
+import imagemin from 'gulp-imagemin';
+import plumber from 'gulp-plumber';
+
+const paths = {
+  dist: 'dist/',
+  src: 'src/',
+  images: {
+    get src () { return `${paths.src}assets/images/*.*`; },
+    get dist () { return `${paths.dist}assets/images/`; }
+  }
+};
+
+const server = browserSync.create();
+
+const reload = (done) => {
+  server.reload();
+  done();
+}
+
+const serve = (done) => {
+  server.init({
+    server: { baseDir: paths.dist },
+    ui: false,
+    open: false,
+    reloadOnRestart: true,
+    notify: false
+  });
+  done();
+}
+
+const clean = () => del([paths.dist]);
+
+const images = () => {
+  return gulp.src(paths.images.src)
+    .pipe(imagemin())
+    .pipe(gulp.dest(paths.images.dist))
+};
+
+const build = () => {
+  return gulp.src(paths.src + '*.pug')
+    .pipe(plumber({
+      errorHandler: notify.onError(function(err) {
+        return {
+           title: err.plugin.toUpperCase(),
+           message: err.message
+        }
+      })
+    }))
+    .pipe(pug())
+    .pipe(mjml())
+    .pipe(gulp.dest(paths.dist))
+};
+
+const watch = () => {
+  gulp.watch(paths.src, gulp.series(build, reload));
+  gulp.watch(paths.images.src, gulp.series(images, reload));
+}
+
+export const dist = gulp.series(clean, build, images);
+const dev = gulp.series(clean, build, images, serve, watch);
+
+export default dev;
---
+++ readme.md
@@ -0,0 +1,22 @@
+# careex
+
+A starter-kit for [mjml](https://mjml.io/documentation/)   
+Powered by [pug](https://pugjs.org/api/getting-started.html) and gulp
+
+### Get your copy
+
+```bash
+git clone git@github.com:in-in/careex.git project && cd project
+```
+
+### Run and watch
+
+```bash
+npm start
+```
+
+### Build project
+
+```bash
+npm run dist
+```
